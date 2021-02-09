3b464c34eb8b6445d8d410a3a051c31e7c79da04
DarkBlue <djfodsjvclkdshvcfijs@lkdlkposaksadksadkad.com>
2021-02-09 18:18:47 +0200

    Tein tason

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
 import './App.css'
-
+import SnakeBoard from './SnakeBoard'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
     Matopeli
       </header>
+      <SnakeBoard />
     </div>
   );
 }
